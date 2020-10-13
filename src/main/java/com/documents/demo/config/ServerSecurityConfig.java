0925b6930489c3aef0ac9fd7482284e838626f6a
zigzagmcquack <eugene.antipenko.s@gmail.com>
2020-10-13 12:56:42 +0300

    change config to access for all requests

---
+++ src/main/java/com/documents/demo/config/ServerSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.documents.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//config class
+
+@Configuration
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors() .and()
+            .csrf().disable() //for disable fake query
+            .authorizeRequests()
+                .anyRequest().permitAll()
+
+        ;
+    }
+}
