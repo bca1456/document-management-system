16e43644c9345a75d51973a73eea401dce1dc231
zigzagmcquack <eugene.antipenko.s@gmail.com>
2020-10-25 21:55:03 +0300

    added swagger annotations

--- src/main/java/com/documents/demo/controller/AdminController.java
+++ src/main/java/com/documents/demo/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.documents.demo.controller;
 
 import com.documents.demo.domain.User;
 import com.documents.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
+@Api
 public class AdminController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @ApiOperation(value = "get all users")
+    @RequestMapping("/all")
     public List<User> allUsers(){
         return userService.findAll();
     }
--- src/main/java/com/documents/demo/controller/DocumentController.java
+++ src/main/java/com/documents/demo/controller/DocumentController.java
@@ -2,6 +2,8 @@ package com.documents.demo.controller;
 
 import com.documents.demo.domain.Document;
 import com.documents.demo.service.DocumentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -11,31 +13,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/document")
+@Api
 public class DocumentController {
 
     @Autowired
     private DocumentService documentService;
 
+    @ApiOperation(value = "get okay string idk for what =)")
     @RequestMapping("/okay")
     public String getOkay(){
         return "okay";
     }
 
+    @ApiOperation(value = "get all documents")
     @RequestMapping(value = "/all")
     public List<Document> findAll(){
        return this.documentService.findAll();
     }
 
+    @ApiOperation(value = "get all documents with pagination")
     @RequestMapping(value = "/all", params = { "page", "size" })
     public Page<Document> findAll(@RequestParam("page") int page, @RequestParam("size") int size){
         return documentService.findPaginated(page, size);
     }
 
+    @ApiOperation(value = "get document by id")
     @RequestMapping("/{document_id}")
     public Document findById(@PathVariable("document_id") int document_id){
         return documentService.findById(document_id);
     }
 
+    @ApiOperation(value = "add document via post")
     @PostMapping("/add")
     public Serializable addDocument(@RequestBody Document document){
 //        System.out.println(document.getCreationDate());
@@ -45,6 +53,7 @@ public class DocumentController {
         return "gg";
     }
 
+    @ApiOperation(value = "delete document by id")
     @DeleteMapping("/{document_id}")
     public Serializable deleteDocument(@PathVariable("document_id") int document_id){
         if (documentService.deleteById(document_id)){
@@ -53,6 +62,7 @@ public class DocumentController {
         return "gg";
     }
 
+    @ApiOperation(value = "update document via put")
     @PutMapping("/update")
     public Serializable updateDocument(@RequestBody Document document){
         if(documentService.update(document)){
